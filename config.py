9cd20bfed789482d823e45f7ca7ff2791033e089
Димитров Иван <124810594+XaiCheremushka@users.noreply.github.com>
2023-05-09 00:00:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
-
 commands = """
     /start - начало рабты бота;
     /help - меню команд;
     /checkRate - проверить текущий курс валюты;
-    /setHighLimit - установить верхний предел (формат записи: "/setHighLimit 65,125");
-    /setLowLimit - установить нижний предел (формат записи: "/setLowLimit 64,125")
-"""
+    /setHighLimit - установить верхний предел 
+    (формат записи: "/setHighLimit 65.125");
+    /setLowLimit - установить нижний предел
+    (формат записи: "/setLowLimit 64.125");
+    /startWork - запустить мониторинг валюты
+    """
 
 token = "6113213624:AAHSSWltRYZyBrYzsz-vGQDSaYJnIRFV5i4"
 
 urlRBC = "https://quote.rbc.ru/ticker/59111"
-
